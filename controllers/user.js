88a4fc70f554747dc9ebe543c634cd304fef3274
Rusev11 <ivaylo.rusev@outlook.com>
2020-11-05 10:21:35 +0200

    User Model Created
    User model created with the following properties:
    - username
    - password
    - chatRooms

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,11 @@ const router = express.Router()
 
 router.post('/login', (request, response, next) => {
     console.log(request);
-    // response.send('test')
+    response.redirect('chat.html')
+})
+
+router.post('/register', (request, response, next) => {
+    console.log(request);
     response.redirect('chat.html')
 })
 
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require ('mongoose')
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+    
+    chatRooms: {
+        type: 'ObjectId',
+        ref: 'ChatRoom'
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)
