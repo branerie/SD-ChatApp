aa37c63bb19faa2165993776815b5109034d3bbb
Rusev11 <ivaylo.rusev@outlook.com>
2020-11-05 12:21:17 +0200

    User model created
    USermodel created.
    Several dependancies added.

--- config/config.js
+++ config/config.js
@@ -1,9 +1,6 @@
-
-
 const config = {
     port: process.env.PORT,
     URL: process.env.URL
-    // `mongodb+srv://rusev:${`123qwe`}@cluster.bsqnt.mongodb.net/ChatApp?retryWrites=true&w=majority`,
     // authCookieName: 'x-auth-token'
 }
 
