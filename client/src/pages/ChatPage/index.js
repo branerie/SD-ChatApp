c0ceb552ec2889011940af2f35d0ed28ab138ddb
gergan ruschev <gergan.ruschev@smartdesign.local>
2020-11-11 16:59:48 +0200

    Created ChatList component

--- client/src/pages/ChatPage/index.js
+++ client/src/pages/ChatPage/index.js
@@ -1,27 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useLocation } from "react-router-dom"
+import ChatList from "../../components/ChatList"
 import io from "socket.io-client"
 import "./index.css"
 
-let socket;
+// let socket;
 
 const ChatPage = () => {
     const location = useLocation()
     const [username, setUsername] = useState(location.username)
     const [socketID, setSocketID] = useState(null)
+    const [groups, setGroups] = useState([])
+    const [confs, setConfs] = useState(['conf', 'conf2', 'conf3']) //dummy 
+    const [chats, setChats] = useState(['user1', 'user2', 'user3']) //dummy
+
+    const socket = useRef()
 
     useEffect(() => {
 
-        socket = io("http://localhost:5000", {
+        socket.current = io("http://localhost:5000", {
             query: { username: location.username },
             transports: ['websocket']
         })
 
-        socket.on("connect", () => {
+        socket.current.on("connect", () => {
             setSocketID(socket.id)
             console.log([socketID, username]);
         })
+
+        socket.current.on('welcome-message', ({ user, msg, groups }) => {
+            welcomeMessage(groups)
+            // let data = {
+            //     time: new Date().toLocaleTimeString(),
+            //     user,
+            //     msg,
+            //     textType: 'text-server',
+            //     windowType: null,
+            //     windowID: "status-window"
+            // }
+            // attachMsg(data)
+            // attachGroups(groups)
+        })
     }, [])
+
+    function welcomeMessage(groups) {
+        setGroups(groups)
+    }
 
     return (
         <div className="chat-container">
@@ -32,21 +56,15 @@ const ChatPage = () => {
             <main className="chat-main">
                 <aside className="chat-sidebar">
                     <ul><li id="status" className="selected">STATUS</li></ul>
-                    <h2>GROUPS</h2>
-                    <ul id="groups"></ul>
-                    <h2>CONFERENCE</h2>
-                    <ul id="conf">
-                        <li>cid1</li>
-                        <li>cid2</li>
-                    </ul>
-                    <h2>CHATS</h2>
-                    <ul id="chat">
-                        <li>Unufri</li>
-                        <li>Цеца</li>
-                    </ul>
+                    {groups ? <ChatList label={"groups"} data={groups} /> : null}
+                    {confs ? <ChatList label={"conferences"} data={confs} /> : null}
+                    {chats ? <ChatList label={"chats"} data={chats} /> : null}
                 </aside>
                 <div className="chat-messages-container">
                     <div className="chat-messages" id="status-window"></div>
+                    {groups.map(group => {
+                        return <div className="chat-messages hidden" id={`group-${group}`}></div>
+                    })}
                 </div>
                 <aside className="chat-sidebar chat-members hidden">
                     <h2>ONLINE</h2>
