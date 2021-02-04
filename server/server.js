f1e475f698d19091c359f4cfe68cbcac9cf849a5
gergan ruschev <gergan.ruschev@smartdesign.local>
2021-02-04 10:50:49 +0200

    Fixed path!

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ require('./config/routes')(app)
 
 if (config.NODE_ENV === "production") {
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, '..client/build/index.html')
+        res.sendFile(path.join(__dirname, '..client/build/index.html'))
     })
 }
 
