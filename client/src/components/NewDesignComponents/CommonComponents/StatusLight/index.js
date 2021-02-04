169eeda3982f7cb3bf214eab8a49c1dd6ad7eb02
gergan ruschev <gergan.ruschev@smartdesign.local>
2021-02-04 12:51:22 +0200

    Temporary logout set in new design

--- client/src/components/NewDesignComponents/CommonComponents/StatusLight/index.js
+++ client/src/components/NewDesignComponents/CommonComponents/StatusLight/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from './index.module.css'
+import { AuthenticateUser } from '../../../../context/authenticationContext'
+
 
 const StatusLight = (props) => {
+    const { logOut } = AuthenticateUser()
     return (
         <div 
             className={`${styles.circle} ${styles[props.color]} ${styles[props.size]}`} 
+            onClick={()=>{logOut()}}
         />
     )
 }
