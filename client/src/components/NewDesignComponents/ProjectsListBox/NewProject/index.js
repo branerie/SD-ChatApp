c44c35c1b5197494058b0b23f53fda1f3d71364b
gergan ruschev <gergan.ruschev@smartdesign.local>
2021-02-10 13:35:08 +0200

    Removed old buttons for creating and joining projects

--- client/src/components/NewDesignComponents/ProjectsListBox/NewProject/index.js
+++ client/src/components/NewDesignComponents/ProjectsListBox/NewProject/index.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styles from './index.module.css'
-import newProjectButton from '../../../../images/newProjectButton.svg'
 import settingsIconBig from '../../../../images/settingsIconBig.svg'
-import joinProjectIcon from '../../../../images/joinProjectIcon.svg'
 import { useHistory } from 'react-router-dom'
-import AddProject from './AddProject'
-import JoinProject from './JoinProject'
 import TransparentBackground from '../../CommonComponents/TransparentBackground'
 import SettingsPage from '../../../../pages/SettingsPage'
 
@@ -13,25 +9,13 @@ const NewProject = () => {
     const history = useHistory()
     const [backgroundOpened, setBackgroundOpened] = useState(false)
     const [settingPageOpened, setSettingPageOpened] = useState(false)
-    const [addProjectOpened, setAddProjectOpened] = useState(false)
-    const [joinProjectOpened, setJoinProjectOpened] = useState(false)
-    
-    const openAddProjectWindow = () => {
-        setBackgroundOpened(true)
-        setAddProjectOpened(true)
-    }
     
     const openSettingsWindow = () => {
         setBackgroundOpened(true)
         setSettingPageOpened(true)
     }
     
-    const openJoinProjectWindow = () => {
-        setBackgroundOpened(true)
-        setJoinProjectOpened(true)
-    }
-    
-    const closeOpenedWindows = () => {
+     const closeOpenedWindows = () => {
         setBackgroundOpened(false)
         setSettingPageOpened(false)
         setAddProjectOpened(false)
@@ -41,12 +25,6 @@ const NewProject = () => {
     return (
         <div>
             <div className={styles['box']}>
-                <div className={styles['button']} onClick={openAddProjectWindow}>
-                    <img src={newProjectButton} className={styles['img']}  />
-                </div>
-                <div className={styles['button']} onClick={openJoinProjectWindow}>
-                    <img src={joinProjectIcon} className={styles['img']}  />
-                </div>
                 <div className={styles['button']} onClick={openSettingsWindow}>
                     <img src={settingsIconBig} className={styles['img']} />
                 </div>
@@ -55,12 +33,6 @@ const NewProject = () => {
                 </div>
                 <div>
                 </div>
-            </div>
-            <div>
-                {addProjectOpened ? <AddProject closeOpenedWindows={closeOpenedWindows} /> : <div />}
-            </div>
-            <div>
-                {joinProjectOpened ? <JoinProject closeOpenedWindows={closeOpenedWindows} /> : <div />}
             </div>
             <div>
                 {backgroundOpened ? <TransparentBackground closeOpenedWindows={closeOpenedWindows}/> : <div />}
